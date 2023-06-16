cb66d27f7a0e6cbd90af09a530fa3d4df714c09b
fandablazek97 <blazek127@gmail.com>
2023-06-16 08:53:06 +0200

    oprava velkých písmen u pozic v referencích

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -90,7 +90,7 @@ export default function Testimonials({
                     level={"none"}
                     size="xs"
                     color="inherit"
-                    className="mb-0 text-left !font-medium text-gray-200"
+                    className="mb-0 text-left !font-medium uppercase text-gray-200"
                   >
                     {testimonial.position}
                   </Heading>
